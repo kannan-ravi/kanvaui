b4a43ea4492d5acfebe3f9a440ceb54a6fed68f1
kannan-ravi <kannanravindran00kr@gmail.com>
2025-12-16 21:11:13 +0530

    chore: initialize kanva ui

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kanva UI | Open Source Shadcn UI Components & Blocks",
+  description:
+    "Open-source Shadcn UI components & Blocks, that you can copy-paste into any TypeScript/Next.js project.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
